4b64613dff06a618f046425e4fee08757d0bb708
daniel luke <manthid5@gmail.com>
2026-09-21 21:21:44 +0300

    Step 3

--- about_me.cpp
+++ about_me.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main() {
-//
+//===================
 // VARIABLE DECLARATION
 // These are the "storage boxes" where we keep user answers
-//
+// QUESTION 1
+//===================
+cout <<"===========" << endl;
+cout <<" WELCOME TO YOUR FIRST PROGRAM!" << endl;
+cout <<"===========" << endl;
+cout << endl;
+
+cout << "1. What is your name? ";
+getline(cin, name); //Reads the entire line of input, including spaces
     return 0;
 }
